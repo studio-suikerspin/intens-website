ee87a60a3df56b3919dc81d8430eed8395a92988
Robin de Laater <robin.delaater@shopmonkey.nl>
2026-01-14 16:45:01 +0100

    Start

--- src/lib/slices/ProjectsGridWithFilters/index.svelte
+++ src/lib/slices/ProjectsGridWithFilters/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Content } from '@prismicio/client';
+	import { isFilled, type Content } from '@prismicio/client';
 	import type { SliceComponentProps } from '@prismicio/svelte';
 
 	type Props = SliceComponentProps<Content.ProjectsGridWithFiltersSlice>;
@@ -7,12 +7,22 @@
 	const { slice }: Props = $props();
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+<section
+	class="projects-grid-section"
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+>
+	<div class="projects-grid">
+		<div class="projects-grid__filters"></div>
+		<div class="projects-grid__projects">
+			{#each slice.primary.projects as { project }, index (index)}
+				{#if isFilled.contentRelationship(project) && project.data}
+					<div class="project-card">
+						<h2 class="project-card__title">{project.data.title}</h2>
+						<p class="project-card__summary">{project.data.summary}</p>
+					</div>
+				{/if}
+			{/each}
+		</div>
+	</div>
 </section>
