813f0dfd4490818cfa95eadef16619eff4a20285
Robin de Laater <robin.delaater@shopmonkey.nl>
2026-01-15 22:07:40 +0100

    Before and after

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -2,9 +2,10 @@ import { browser } from '$app/environment';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { Draggable } from 'gsap/Draggable';
 
 if (browser) {
-	gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother, Draggable);
 }
 
-export { gsap, ScrollTrigger, ScrollSmoother };
+export { gsap, ScrollTrigger, ScrollSmoother, Draggable };
