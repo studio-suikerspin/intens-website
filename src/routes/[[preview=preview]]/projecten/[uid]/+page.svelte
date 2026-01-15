a55591010642cd459f0e94883bc3ba268cf15f1c
Robin de Laater <robin@suikerspin.studio>
2026-01-15 20:43:17 +0100

    Project grid with filters v1 (#23)

---
+++ src/routes/[[preview=preview]]/projecten/[uid]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { SliceZone } from '@prismicio/svelte';
+	import type { PageProps } from './$types';
+
+	import { components } from '$lib/slices';
+
+	const { data }: PageProps = $props();
+	console.log(data);
+</script>
